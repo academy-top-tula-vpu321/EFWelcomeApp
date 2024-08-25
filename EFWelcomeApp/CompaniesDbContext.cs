2300959012d08a2c81901c2af9f6e479607b745b
Asteron2007 <52139280+Asteron2007@users.noreply.github.com>
2024-08-25 13:21:36 +0300

    Добавьте файлы проекта.

---
+++ EFWelcomeApp/CompaniesDbContext.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.EntityFrameworkCore.Diagnostics;
+
+namespace EFWelcomeApp
+{
+    public class CompaniesDbContext : DbContext
+    {
+        //readonly StreamWriter logWriter = new StreamWriter("log.txt", true);
+        public DbSet<Employee> Employees { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+           
+            base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseSqlServer(@"Data Source=Y5-0\MSSQLSERVER01;Initial Catalog=CompaniesDb;Integrated Security=True;Connect Timeout=30;Encrypt=False;Trust Server Certificate=True;Application Intent=ReadWrite;Multi Subnet Failover=False");
+            //optionsBuilder.LogTo(Console.WriteLine, new[] { RelationalEventId.CommandExecuted });
+ 
+            //optionsBuilder.UseSqlServer(connectionString);
+
+        //    var config = new ConfigurationBuilder()
+        //                    .AddJsonFile("appsettings.json")
+        //                    .SetBasePath(Directory.GetCurrentDirectory())
+        //                    .Build();
+
+        //    optionsBuilder.UseSqlServer(config.GetConnectionString("DefaultConnection"));
+        }
+    }
+}
---
+++ EFWelcomeApp/Program.cs
@@ -0,0 +1,43 @@
+﻿using EFWelcomeApp;
+
+using (CompaniesDbContext context = new())
+{
+    //context.Database.EnsureDeleted();
+    //context.Database.EnsureCreated();
+
+    // CRUD - Create (Insert)
+    //Employee sam = new() { Name = "Sammy", DateBirth = new DateTime(2001, 6, 17) };
+    //Employee jim = new() { Name = "Jimmy", DateBirth = new DateTime(1997, 3, 2) };
+    //context.Employees.Add(sam);
+    //context.Employees.Add(jim);
+
+    // CRUD - Update (Update)
+    //int id = 3;
+    //var employee = context.Employees.FirstOrDefault(e => e.Id == id);
+
+    //if (employee is not null)
+    //    employee.DateBirth = new DateTime(2001, 9, 25);
+
+    // CRUD - Delete (Delete)
+    //int id = 3;
+    //var employee = context.Employees.FirstOrDefault(e => e.Id == id);
+
+    //if (employee is not null)
+    //    context.Employees.Remove(employee);
+
+    //context.SaveChanges();
+}
+
+using (CompaniesDbContext context = new())
+{
+
+    // CRUD - Read (Select)
+    //if (!context.Database.CanConnect()) return;
+
+    var employees = context.Employees.ToList();
+    //Console.WriteLine("Employees:");
+
+    //foreach (var employee in employees)
+    //    Console.WriteLine(employee);
+}
+
